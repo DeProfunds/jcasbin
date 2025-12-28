5de420f7f1296097be5d13cc5fc16520316bc84e
github-action-benchmark <github@users.noreply.github.com>
2025-12-28 21:58:43 +0000

    add JCasbin Benchmark (jmh) benchmark result for 907ae352d8265816581f7a6e6155373a2c83f01d

--- bench/data.js
+++ bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1766958926750,
+  "lastUpdate": 1766959123198,
   "repoUrl": "https://github.com/DeProfunds/jcasbin",
   "entries": {
     "JCasbin Benchmark": [
@@ -54,6 +54,62 @@ window.BENCHMARK_DATA = {
           {
             "name": "org.casbin.jcasbin.main.benchmark.BenchmarkMemory.measureMemory ( {\"scenario\":\"RBAC_Medium\"} )",
             "value": 395.71605033333327,
+            "unit": "ms/op",
+            "extra": "iterations: 3\nforks: 1\nthreads: 1"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "name": "DeProfunds",
+            "username": "DeProfunds"
+          },
+          "committer": {
+            "name": "DeProfunds",
+            "username": "DeProfunds"
+          },
+          "id": "907ae352d8265816581f7a6e6155373a2c83f01d",
+          "message": "Benchmark/pr10 graph test",
+          "timestamp": "2025-12-28T20:08:58Z",
+          "url": "https://github.com/DeProfunds/jcasbin/pull/14/commits/907ae352d8265816581f7a6e6155373a2c83f01d"
+        },
+        "date": 1766959122269,
+        "tool": "jmh",
+        "benches": [
+          {
+            "name": "org.casbin.jcasbin.main.benchmark.BenchmarkEnforce.enforce ( {\"dataScale\":\"medium\",\"modelType\":\"rbac\",\"useCache\":\"false\"} )",
+            "value": 2.103373157811225,
+            "unit": "ops/ms",
+            "extra": "iterations: 3\nforks: 1\nthreads: 1"
+          },
+          {
+            "name": "org.casbin.jcasbin.main.benchmark.BenchmarkManagement.addPolicy ( {\"currentRuleSize\":\"10000\"} )",
+            "value": 870.2600251586008,
+            "unit": "ops/ms",
+            "extra": "iterations: 3\nforks: 1\nthreads: 1"
+          },
+          {
+            "name": "org.casbin.jcasbin.main.benchmark.BenchmarkManagement.hasPolicy ( {\"currentRuleSize\":\"10000\"} )",
+            "value": 5913.010907545909,
+            "unit": "ops/ms",
+            "extra": "iterations: 3\nforks: 1\nthreads: 1"
+          },
+          {
+            "name": "org.casbin.jcasbin.main.benchmark.BenchmarkManagement.removePolicy ( {\"currentRuleSize\":\"10000\"} )",
+            "value": 0.9636246741709117,
+            "unit": "ops/ms",
+            "extra": "iterations: 3\nforks: 1\nthreads: 1"
+          },
+          {
+            "name": "org.casbin.jcasbin.main.benchmark.BenchmarkManagement.updatePolicy ( {\"currentRuleSize\":\"10000\"} )",
+            "value": 5675.948463173697,
+            "unit": "ops/ms",
+            "extra": "iterations: 3\nforks: 1\nthreads: 1"
+          },
+          {
+            "name": "org.casbin.jcasbin.main.benchmark.BenchmarkMemory.measureMemory ( {\"scenario\":\"RBAC_Medium\"} )",
+            "value": 388.5113836666667,
             "unit": "ms/op",
             "extra": "iterations: 3\nforks: 1\nthreads: 1"
           }
